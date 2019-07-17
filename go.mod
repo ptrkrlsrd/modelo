f4a930f61e35e6a371f786cca2d39beef18813ff
Petter Karlsrud <petterkarlsrud@me.com>
2019-07-17 14:32:00 +0200

    Initial

---
+++ go.mod
@@ -0,0 +1,14 @@
+module templater
+
+go 1.12
+
+require (
+	github.com/AlecAivazis/survey/v2 v2.0.1
+	github.com/shurcooL/githubv4 v0.0.0-20190625031733-ee671ab25ff0
+	github.com/shurcooL/graphql v0.0.0-20181231061246-d48a9a75455f // indirect
+	github.com/src-d/go-git v4.7.0+incompatible
+	golang.org/x/oauth2 v0.0.0-20190604053449-0f29369cfe45
+	golang.org/x/sync v0.0.0-20190227155943-e225da77a7e6 // indirect
+	gopkg.in/src-d/go-billy.v4 v4.3.1 // indirect
+	gopkg.in/src-d/go-git.v4 v4.12.0
+)
---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/AlecAivazis/survey/v2"
+	"github.com/shurcooL/githubv4"
+	"golang.org/x/oauth2"
+	"gopkg.in/src-d/go-git.v4"
+)
+
+var query struct {
+	Viewer struct {
+		Login          githubv4.String
+		CreatedAt      githubv4.DateTime
+		IsBountyHunter githubv4.Boolean
+		BioHTML        githubv4.HTML
+		WebsiteURL     githubv4.URI
+	}
+}
+
+type Repository struct {
+	Name       string
+	URL        string
+	IsTemplate bool
+	IsPrivate  bool
+}
+
+func main() {
+	src := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: os.Getenv("GITHUB_TOKEN")},
+	)
+	httpClient := oauth2.NewClient(context.Background(), src)
+
+	client := githubv4.NewClient(httpClient)
+	var q struct {
+		Viewer struct {
+			Name         string
+			Repositories struct {
+				Nodes []Repository
+			} `graphql:"repositories(first: 100)"`
+		}
+	}
+
+	err := client.Query(context.Background(), &q, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dir, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var templates []string
+	var repositories = q.Viewer.Repositories.Nodes
+	for _, v := range q.Viewer.Repositories.Nodes {
+		if v.IsTemplate && !v.IsPrivate {
+			templates = append(templates, v.Name)
+		}
+	}
+
+	answers := struct {
+		Name     string
+		Template string
+	}{}
+
+	var qs = []*survey.Question{
+		{
+			Name: "Template",
+			Prompt: &survey.Select{
+				Message: "Choose a template:",
+				Options: templates,
+			},
+		},
+	}
+
+	err = survey.Ask(qs, &answers)
+	if err != nil {
+		log.Println(err.Error())
+		return
+	}
+
+	for _, v := range repositories {
+		if v.Name == answers.Template {
+			root := dir + "/" + answers.Name
+			_, err := git.PlainClone(root, false, &git.CloneOptions{
+				URL:      v.URL,
+				Progress: os.Stdout,
+			})
+
+			if err != nil {
+				log.Println(err.Error())
+				return
+			}
+		}
+	}
+}
