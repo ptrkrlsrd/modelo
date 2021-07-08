f8e8e9816d58f25cea1eb7e7f2aa759bf25bf09c
Petter K <ptrkrlsrd@users.noreply.github.com>
2021-07-08 22:57:32 +0200

    Started creating tests (#10)
    * Started creating tests

---
+++ pkg/github/repository_test.go
@@ -0,0 +1,107 @@
+package github
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestRepositories_GetTemplates(t *testing.T) {
+	tests := []struct {
+		name         string
+		repositories Repositories
+		want         Repositories
+	}{
+		{
+			repositories: Repositories{
+				Repository{
+					Name:       "non-template repo",
+					IsTemplate: false,
+				},
+				Repository{
+					Name:       "template repo",
+					IsTemplate: true,
+				},
+			},
+			want: Repositories{
+				Repository{
+					Name:       "template repo",
+					IsTemplate: true,
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.repositories.GetTemplates(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Repositories.GetTemplates() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestRepositories_GetNames(t *testing.T) {
+	tests := []struct {
+		name                string
+		repositories        Repositories
+		wantRepositoryNames []string
+	}{
+		{
+			name: "get names from repositories name",
+			repositories: Repositories{
+				{
+					Name: "Repo 1",
+				},
+				{
+					Name: "Repo 2",
+				},
+			},
+			wantRepositoryNames: []string{"Repo 1", "Repo 2"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if gotRepositoryNames := tt.repositories.GetNames(); !reflect.DeepEqual(gotRepositoryNames, tt.wantRepositoryNames) {
+				t.Errorf("Repositories.GetNames() = %v, want %v", gotRepositoryNames, tt.wantRepositoryNames)
+			}
+		})
+	}
+}
+
+func TestRepositories_FindByName(t *testing.T) {
+	type args struct {
+		name string
+	}
+	tests := []struct {
+		name         string
+		repositories Repositories
+		args         args
+		want         Repository
+		wantErr      bool
+	}{
+		{
+			name: "can find repo by name",
+			repositories: Repositories{
+				{
+					Name: "Repo 1",
+				},
+			},
+			args: args{
+				name: "Repo 1",
+			},
+			want:    Repository{Name: "Repo 1"},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := tt.repositories.FindByName(tt.args.name)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Repositories.FindByName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Repositories.FindByName() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
---
+++ pkg/github/service_test.go
@@ -0,0 +1,37 @@
+package github
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestNewService(t *testing.T) {
+	type args struct {
+		githubUsername string
+		githubToken    string
+	}
+	tests := []struct {
+		name string
+		args args
+		want Service
+	}{
+		{
+			name: "Can make a new service",
+			args: args{githubUsername: "github_username", githubToken: "github_token"},
+			want: Service{
+				GithubClient: newClient("github_token"),
+				auth: Auth{
+					Username: "github_username",
+					Token:    "github_token",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewService(tt.args.githubUsername, tt.args.githubToken); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewService() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
